2c20582cd3c5fdbb356ea7d58469fc4a78001f4e
r-falcon <rfalcon@163.com>
2022-11-16 16:45:28 +0800

    -

--- src/views/basic/setup/component.vue
+++ src/views/basic/setup/component.vue
@@ -1,22 +1,36 @@
 <template>
   <div>
-    <!-- 
-    1.setup函数为使用composition api提供了统一的入口
-    2.setup函数在beforeCreate、created钩子之前执行，vue3中废除了这两个钩子;因为此时vue对象还没有建立，所以vue3无法访问this，是undefined（如何访问this？vue中的getCurrentInstance方法返回了proxy和ctx）
-    3.setup函数的参数:props[父组件传递过来的值],context{attrs[没有被props定义的属性],slots[插槽],emit[触发事件]}
-    4.【注】子组件在接受attrs时：只有一个根元素的情况下，默认绑定到根元素；有两个根元素的情况下，默认都不绑定，需要使用在对应的根元素上添加v-bind="$attrs"
-    5.子组件通过expose向父组件暴露方法或属性，父组件通过在子组件定义ref调用子组件的方法或属性
-    6.setup函数中return的内容，可以在模板中直接使用，包括变量和方法
-    7.setup函数中的数据不具备响应式，使其具有响应式，使用ref和reactive等定义使用
-   -->
-    <!-- <div>{{ msg }}-只有一个根元素，默认绑定</div>
-    <div v-bind="$attrs">{{ msg }}有两个以上根元素,绑定所有attrs</div>
-    <div v-bind="$attrs.style">{{ msg }}有两个以上根元素,绑定attrs特定属性</div> -->
     <p>这是子组件</p>
     <p>接受的信息：msg:{{ msg }},count:{{ $attrs.count }}</p>
     <el-button type="primary" @click="handleClick">change msg</el-button>
     <el-button @click="$attrs.onAddCount()">add count</el-button>
     <slot />
+
+    <div style="margin-top: 20px; color: red">
+      <p>1.setup函数为使用composition api提供了统一的入口</p>
+      <p>
+        2.setup函数在beforeCreate、created钩子之前执行，vue3中废除了这两个钩子;因为此时vue对象还没有建立，所以vue3无法访问this，是undefined（如何访问this？vue中的getCurrentInstance方法返回了proxy和ctx）
+      </p>
+      <p>
+        3.setup函数的参数:props[父组件传递过来的值],context{attrs[没有被props定义的属性],slots[插槽],emit[触发事件]}
+      </p>
+      <p>
+        4.【注】子组件在接受attrs时：只有一个根元素的情况下，默认绑定到根元素；有两个根元素的情况下，默认都不绑定，需要使用在对应的根元素上添加v-bind="$attrs"
+      </p>
+      <!--  <div>{{ msg }}-只有一个根元素，默认绑定</div>
+            <div v-bind="$attrs">{{ msg }}有两个以上根元素,绑定所有attrs</div>
+            <div v-bind="$attrs.style">{{ msg }}有两个以上根元素,绑定attrs特定属性</div> -->
+      <p>
+        5.子组件通过expose向父组件暴露方法或属性，父组件通过在子组件定义ref调用子组件的方法或属性
+      </p>
+      <p>6.setup函数中return的内容，可以在模板中直接使用，包括变量和方法</p>
+      <p>
+        7.setup函数中的数据不具备响应式，使其具有响应式，使用ref和reactive等定义使用
+      </p>
+      <p>
+        8.setup语法糖：defineProps、defineEmits、defineExpose、useSlots、useAttrs
+      </p>
+    </div>
   </div>
 </template>
 
